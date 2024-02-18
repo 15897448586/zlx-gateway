c3bfad578ae03f32e4674c39a01bfba05ae0492f
BG270 <BG270@DESKTOP-KGTGMJJ>
2024-02-18 18:02:21 +0800

    Initial commit

---
+++ src/test/java/com/zlx/zlxgateway/ZlxGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlx.zlxgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZlxGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
